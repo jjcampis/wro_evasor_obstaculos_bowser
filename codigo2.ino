9c24f19fa394ab9bab579110455f3ed4f88d3165
jjcampis <jjcampis@gmail.com>
2023-09-21 21:53:00 -0300

    añadido codigo 2

--- codigo2.ino
+++ codigo2.ino
@@ -5,22 +5,22 @@
 int distanciaInicialIzq = 0;
 int distanciaInicialDer = 0;
 // Definir pines para los motores L298
-const int motor1 = 2;
-const int motor2 = 4;
-const int pwm = 3;
+const int motor1 = 7;
+const int motor2 = 5;
+const int pwm = 6;
 //para los giros y avances
 unsigned long tiempoInicioAvance = 0;  // Variable para controlar el tiempo de avance
 const unsigned long duracionAvance = 10000;  // Duración de avance en milisegundos (5 segundos)
 
 // Definir pines para el servo SG90
-const int pinServo = 6;
+const int pinServo = 11;
 Servo servo;
 
 // Definir pines para los sensores de ultrasonido
-const int TRIG_PIN_1 = 7;
-const int ECHO_PIN_1 = 8;
-const int TRIG_PIN_2 = 9;
-const int ECHO_PIN_2 = 10;
+const int TRIG_PIN_1 = 12;//izq
+const int ECHO_PIN_1 = 13;
+const int TRIG_PIN_2 = 8;//der
+const int ECHO_PIN_2 = 9;
 
 // Definir objetos de los sensores de ultrasonido
 NewPing s_Izq(TRIG_PIN_1, ECHO_PIN_1, 400);
@@ -86,26 +86,43 @@ avanzar(255);
 
 
   // Imprimir información en la comunicación serial
-  Serial.print("Distancia Izquierda: ");
+  // Imprimir los resultados en una tabla mediante Serial
+  Serial.print("Ssr Izq:");
+  Serial.print(distanciaIzq);
+  Serial.print("\tSsr Der:");
+  Serial.print(distanciaDer);
+  Serial.print(" cm");
+  Serial.print("\tdiff: ");
+  Serial.print(diferenciaDistancias);
+  Serial.print(" cm");
+  Serial.print("\tVueltas: ");
+  Serial.print(contadorVueltas);
+  Serial.print("\tGiros: ");
+  Serial.println(contadorGiros);
+  /*Serial.print("Distancia Izquierda: ");
   Serial.print(distanciaIzq);
   Serial.print(" cm, Distancia Derecha: ");
   Serial.print(distanciaDer);
+  Serial.print(" cm, diferencia: ");
+  Serial.print(diferenciaDistancias);
+  Serial.println(" cm");
   Serial.print(" cm, Vueltas: ");
   Serial.print(contadorVueltas);
   Serial.print(", Giros: ");
-  Serial.println(contadorGiros);
+  Serial.println(contadorGiros);*/
   
   delay(1000);  // Esperar 1 segundo antes de la siguiente medición
 }
 
 void giraIzquierda() {
-  servo.write(180);
+  servo.write(0);
 }
 
 void giraDerecha() {
-  servo.write(0);
+  servo.write(180);
 }
 void avanzar(int pot) {
+  servo.write(75);
   analogWrite(pwm,pot);
  digitalWrite(motor1,HIGH);  
  digitalWrite(motor2,LOW);  
